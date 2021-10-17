d1d2890b8734a79114ba34dd46ab7e82de02cc53
Vikash Mehta <vikashkumar02jan@gmail.com>
2021-10-17 13:10:52 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        
+        if (buttonText == 'C') {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+            
+        }
+
+        else if(buttonText == 'D'){
+            screen.value = screen.value.slice(0, -1);
+            screenValue =screen.value;
+            
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    })
+}
+
